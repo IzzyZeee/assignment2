2b08f848fa7c9eaec413bb9af5de01c5af1dce59
Isadora Zhang <izzyorsomething@gmail.com>
2026-03-02 22:49:58 -0500

    Basically finished top half of UI. Started tweaking around with the Canvas API and how to get outputs to show upon click. Need to make bottom half UI rectangle horizontally fit the top, and for table to be less ugly, and ADD THE SINGLE LINE OF THE CUBIC EQUATION.

--- src/counter.ts
+++
@@ -1,9 +0,0 @@
-export function setupCounter(element: HTMLButtonElement) {
-  let counter = 0
-  const setCounter = (count: number) => {
-    counter = count
-    element.innerHTML = `skigumize it ${counter} times`
-  }
-  element.addEventListener('click', () => setCounter(counter + 1))
-  setCounter(0)
-}
--- src/main.ts
+++ src/main.ts
@@ -1,54 +1,71 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import './calculation.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <html>
-    <div class="main_ui">
+    <div class="input_ui">
       <h1 class="h1"> Cubic Solver </h1>
-      <body>
-        <form>
-          <input type="text"> a-value: </input>
-          <input type="text"> b-value: </input>
-          <input type="text"> c-value: </input>
-          <input type="text"> d-value: </input>
-          <br>
-          <input type="submit" value="Solve that cubic!"></input>
-          <input type="reset" value="Clear"></input>
-        <form>
-      </body>
+        <div>
+          <form class="form font" id="solve"> 
+            <div class="input_item">
+              a-value: <br>
+              <input type="text" class="form_input" id="a_val">
+            </div>
+            <div class="input_item">
+              b-value: <br>
+              <input type="text" class="form_input" id="b_val">
+            </div>
+            <div class="input_item">
+              c-value: <br>
+              <input type="text" class="form_input" id="c_val">
+            </div>
+            <div class="input_item">
+              d-value: <br>
+              <input type="text" class="form_input" id="d_val">
+            </div>
+            <input type="submit" class="button font" value="Solve that cubic!">
+            <input type="reset" class="button font" value="Clear">
+          <form>
+        </div>
     </div>
-    <div class="main_ui">
+      <label for="result">Result:</label>
+      <input type="text" id="result" name="result" />
+    <div>
+      <canvas0 id="graph" width="600" height="400"></canvas>
+    </div>
+    <div class="output_ui">
       <body>
-        <p>(Display cubic written out here)</p>
-        <table>
-          <tr>
-            <td>p</td>
-            <td>p val</td>
+        <table class="font">
+          <tr class="table_row">
+            <td class="top_col_1">p</td>
+            <td class="top_col_2">pval</td>
           </tr>
-          <tr>
+          <tr class="table_row">
             <td>q</td>
-            <td>q val</td>
+            <td>qval</td>
+          </tr>
+          <tr class="table_row">
+            <td>discriminant</td>
+            <td>dval</td>
           </tr>
         </table>
-        <table>
-          <tr>
-            <td>Value</td>
-            <td>x</td>
-            <td>y</td>
+        <table class="font">
+          <tr class="table_row colored_row" >
+            <td class="bottom_col_1">Value</td>
+            <td class="bottom_col_2">x</td>
+            <td class="bottom_col_3">y</td>
           </tr>
-          <tr>
+          <tr class="table_row">
             <td>Root 1</td>
             <td>x val</td>
             <td>0</td>
           </tr>          
-          <tr>
+          <tr class="table_row">
             <td>Root 2</td>
             <td>x val</td>
             <td>0</td>
           </tr>
-          <tr>
+          <tr class="table_row">
             <td>Root 3</td>
             <td>x val</td>
             <td>zero</td>
@@ -58,5 +75,46 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     </div>
   </html>
 `
+const form = document.getElementById("solve") as HTMLFormElement;
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+form?.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+    // const a: number = Number(FormData.get("a_val"));
+    // const b: number = Number(formData.get("b"));
+    // const c: number = Number(formData.get("c"));
+
+  (document.getElementById("result") as HTMLInputElement).value = "No Roots";
+
+  const canvas = document.getElementById("graph");
+  const ctx = canvas.getContext("2d");
+
+  ctx.moveTo(50, 100); // starting point
+  ctx.lineTo(300, 200); // ending point
+  ctx.stroke(); // actually draw it
+
+  ctx.beginPath();
+  ctx.moveTo(50, 100);
+  ctx.lineTo(150, 120);
+  ctx.lineTo(250, 80);
+  ctx.lineTo(350, 140);
+  ctx.stroke();
+    // const formData = new FormData(form);
+
+    // const a: number = Number(formData.get("a"));
+    // const b: number = Number(formData.get("b"));
+    // const c: number = Number(formData.get("c"));
+
+    // const discriminant = b * b - 4 * a * c;
+
+    // if (discriminant < 0) {
+    //     (document.getElementById("result") as HTMLInputElement).value = "No Roots";
+    // } else if (discriminant > 0) {
+    //     const rootOne = (-b + Math.sqrt(discriminant)) / (2 * a);
+    //     const rootTwo = (-b - Math.sqrt(discriminant)) / (2 * a);
+    //     (document.getElementById("result") as HTMLInputElement).value = `x1=${rootOne}, x2=${rootTwo}`;
+    // } else {
+    //     const rootOne = (-b + Math.sqrt(discriminant)) / (2 * a);
+    //     (document.getElementById("result") as HTMLInputElement).value = `x=${rootOne}`;
+    // }
+})
