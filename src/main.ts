f6801a1341ab129b8043efc8791a42c35ef4dd2b
Isadora Zhang <izzyorsomething@gmail.com>
2026-03-03 22:13:44 -0500

    Wrote all calculations for cubic and also linked input and output

--- src/main.ts
+++ src/main.ts
@@ -6,25 +6,29 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     <div class="input_ui">
       <h1 class="h1"> Cubic Solver </h1>
         <div>
-          <form class="form font" id="solve"> 
-            <div class="input_item">
-              a-value: <br>
-              <input type="text" class="form_input" id="a_val">
+          <form class="form font" id="cubic"> 
+            <div>
+              <div class="input_item">
+                a-value: <br>
+                <input type="number" class="form_input" name="a_val" required>
+              </div>
+              <div class="input_item">
+                b-value: <br>
+                <input type="number" class="form_input" name="b_val" required>
+              </div>
+              <div class="input_item">
+                c-value: <br>
+                <input type="number" class="form_input" name="c_val" required>
+              </div>
+              <div class="input_item">
+                d-value: <br>
+                <input type="number" class="form_input" name="d_val" required>
+              </div>
             </div>
-            <div class="input_item">
-              b-value: <br>
-              <input type="text" class="form_input" id="b_val">
-            </div>
-            <div class="input_item">
-              c-value: <br>
-              <input type="text" class="form_input" id="c_val">
-            </div>
-            <div class="input_item">
-              d-value: <br>
-              <input type="text" class="form_input" id="d_val">
-            </div>
-            <input type="submit" class="button font" value="Solve that cubic!">
-            <input type="reset" class="button font" value="Clear">
+            <div>
+              <input type="submit" class="button font" value="Solve that cubic!">
+              <input type="reset" class="button font" value="Clear">
+            </div>     
           <form>
         </div>
     </div>
@@ -75,46 +79,57 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     </div>
   </html>
 `
-const form = document.getElementById("solve") as HTMLFormElement;
+const form = document.getElementById("cubic") as HTMLFormElement; /* First identify form by id ("cubic") */
 
 form?.addEventListener("submit", (event) => {
   event.preventDefault();
+  const formData = new FormData(form); /* Create object from FormData class that allows you to use it */
 
-    // const a: number = Number(FormData.get("a_val"));
-    // const b: number = Number(formData.get("b"));
-    // const c: number = Number(formData.get("c"));
+  const a = Number(formData.get("a_val"));
+  const b = Number(formData.get("b_val"));
+  const c = Number(formData.get("c_val"));
+  const d = Number(formData.get("d_val"));
 
-  (document.getElementById("result") as HTMLInputElement).value = "No Roots";
+  const p = (3 * a * c - b * b) / (3 * a * a);
+  const q = (27 * a * a * d - 9 * a * b * c + 2 * b * b * b) / (27 * a * a * a);
+  const delta = Math.pow((q / 2), 2) + Math.pow((p / 3), 3);
 
-  const canvas = document.getElementById("graph");
-  const ctx = canvas.getContext("2d");
+  let x1 = null;
+  let x2 = null;
+  let x3 = null;
 
-  ctx.moveTo(50, 100); // starting point
-  ctx.lineTo(300, 200); // ending point
-  ctx.stroke(); // actually draw it
+  function cardano(a: number, b: number, p: number, q: number) { /* Calculates a single root */
+    return Math.cbrt(-q / 2 + Math.sqrt(Math.pow((q / 2), 2) + Math.pow((p / 3), 3))) + Math.cbrt(-q / 2 - Math.sqrt(Math.pow((q / 2), 2) + Math.pow((p / 3), 3))) - b / (3 * a);
+  }
 
-  ctx.beginPath();
-  ctx.moveTo(50, 100);
-  ctx.lineTo(150, 120);
-  ctx.lineTo(250, 80);
-  ctx.lineTo(350, 140);
-  ctx.stroke();
-    // const formData = new FormData(form);
+  if (Math.abs(delta) < 1e-15) { /* Case C: Delta equals 0, but sometimes the computer can't actually get the zero so it becomes very close to zero, which we detect under the threshold (between 0 and 1e-15) */
 
-    // const a: number = Number(formData.get("a"));
-    // const b: number = Number(formData.get("b"));
-    // const c: number = Number(formData.get("c"));
+    if (p == 0 && q == 0) { /* Case C1: Triple root when p = q = 0 */
+      x1 = cardano(a, b, p, q);      
+      x2 = x1;
+      x3 = x1;
+    } else { /* Case C2: Double root */
+      x1 = cardano(a, b, p, q); /* Double */
+      x2 = Math.cbrt(q / 2) - b / (3 * a); /* Single */
+      x3 = x2;
+    }
 
-    // const discriminant = b * b - 4 * a * c;
+  } else if (delta < 0) { /* Case A: 3 real roots */
 
-    // if (discriminant < 0) {
-    //     (document.getElementById("result") as HTMLInputElement).value = "No Roots";
-    // } else if (discriminant > 0) {
-    //     const rootOne = (-b + Math.sqrt(discriminant)) / (2 * a);
-    //     const rootTwo = (-b - Math.sqrt(discriminant)) / (2 * a);
-    //     (document.getElementById("result") as HTMLInputElement).value = `x1=${rootOne}, x2=${rootTwo}`;
-    // } else {
-    //     const rootOne = (-b + Math.sqrt(discriminant)) / (2 * a);
-    //     (document.getElementById("result") as HTMLInputElement).value = `x=${rootOne}`;
-    // }
+    const theta = (1 / 3) * Math.acos(-q / (2 * Math.sqrt(-Math.pow((p / 3), 3))));
+    x1 = 2 * Math.sqrt(-p / 3) * Math.cos(theta) - b / (3 * a);
+    x2 = 2 * Math.sqrt(-p / 3) * Math.cos(theta + 2 * Math.PI / 3) - b / (3 * a);
+    x3 = 2 * Math.sqrt(-p / 3) * Math.cos(theta + 4 * Math.PI / 3) - b / (3 * a);
+  
+  } else { /* Case B: Delta > 0, 1 real root and 2 complex roots */
+
+    x1 = cardano(a, b, p, q);
+    x2 = "Complex Number";
+    x3 = "Complex Number";
+
+  } 
+
+// (document.getElementById("result") as HTMLInputElement).value = `x1 = ${x1}, x2 = ${x2}, x3 = ${x3}, delta = ${delta}`;
+(document.getElementById("result") as HTMLInputElement).value = `x1 = ${x1}, x2 = ${x2}, x3 = ${x3}, delta = ${delta}`;
+
 })
